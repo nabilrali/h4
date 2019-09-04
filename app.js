c77e243e1c4d6834b1c5c2b9c4a700bbb7dfd0d4
boudlal <boudlal98@gmail.com>
2019-09-04 16:37:11 +0100

    add tamper script

--- app.js
+++ app.js
@@ -70,38 +70,38 @@ app.get('/login', (req, res) => {
 })
 
 
-// app.get('/', (req, res) => {
-//   console.log(req.query);
-//   let {target_user } = req.query
-//   if (target_user.includes("http") || target_user.includes("instagram")) {
-//     console.log('yep');
-//     target_user = target_user.split('.com/')[1].replace('/', '')
-//   }
-//   if (target_user.includes("?")) {
-//     target_user = target_user.split('?')[0].replace('/', '')
-//   }
-//   target_user = target_user.trim().toString();
-//   console.log('target_user', target_user);
-//   return Instagram.getUserDataByUsername(target_user).then((t) =>
-//   {
-//     if (t.graphql.hasOwnProperty('user')) {
-//       let user_id = t.graphql.user.id
-//       let data = {
-//         quantity: parseInt(req.query.quantity),
-//         user_id: parseInt(user_id)
-//       }
-//       console.log('data', data);
-//       controller.getFollowers(data, (err, result) => {
-//         if (err) res.json(err);
-//         res.json(result)
-//       })
-//     }
-//
-//     // return Instagram.getUserFollowers(t.graphql.user.id).then((t) =>
-//     // {
-//     //   console.log(t); // - instagram followers for user "username-for-get"
-//     // })
-//   }).catch((err) => {
-//     return res.json({success: false, message: "something wrong with that user "+ target_user, err: err})
-//   });
-// })
+app.get('/', (req, res) => {
+  console.log(req.query);
+  let {target_user } = req.query
+  if (target_user.includes("http") || target_user.includes("instagram")) {
+    console.log('yep');
+    target_user = target_user.split('.com/')[1].replace('/', '')
+  }
+  if (target_user.includes("?")) {
+    target_user = target_user.split('?')[0].replace('/', '')
+  }
+  target_user = target_user.trim().toString();
+  console.log('target_user', target_user);
+  return Instagram.getUserDataByUsername(target_user).then((t) =>
+  {
+    if (t.graphql.hasOwnProperty('user')) {
+      let user_id = t.graphql.user.id
+      let data = {
+        quantity: parseInt(req.query.quantity),
+        user_id: parseInt(user_id)
+      }
+      console.log('data', data);
+      controller.getFollowers(data, (err, result) => {
+        if (err) res.json(err);
+        res.json(result)
+      })
+    }
+
+    // return Instagram.getUserFollowers(t.graphql.user.id).then((t) =>
+    // {
+    //   console.log(t); // - instagram followers for user "username-for-get"
+    // })
+  }).catch((err) => {
+    return res.json({success: false, message: "something wrong with that user "+ target_user, err: err})
+  });
+})
