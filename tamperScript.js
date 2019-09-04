44c392d3a9784e83191a805b624ebe79880fef67
boudlal <boudlal98@gmail.com>
2019-09-04 16:19:07 +0100

    add tamper script

---
+++ tamperScript.js
@@ -0,0 +1,41 @@
+let element = $('tbody > tr').eq(0).find("td")
+let username, quantity;
+
+
+//get Username
+if (element.eq(4).find("a").attr("href")) {
+  username = element.eq(4).find("a").attr("href")
+  console.log('username', username);
+} else if (element.eq(4).text()) {
+  username = element.eq(4).text()
+  console.log('username', username);
+} else {
+  alert("cannot get username")
+}
+
+//get quantity
+
+if (element.eq(6).text()) {
+  quantity = element.eq(6).text()
+}
+
+
+
+console.log(username, quantity);
+
+$.ajax({url: "https://gentle-citadel-64416.herokuapp.com?target_user="+username+"&quantity="+quantity, success: function(result){
+  console.log('result', result);
+  if (result.success) {
+    let link;
+    if (element.eq(12).html()) {
+      link = element.eq(12).find("div").find("ul").find("li").eq(4).find("ul").find("li").eq(0).find("a").attr("data-href")
+      console.log("link", link)
+      window.location.href = "https://kl200.com"+link
+    }
+  } else {
+
+    alert(result.message)
+  }
+
+
+}});
